c790491549ba5eaf2f177301b787437e743f3644
murka91 <166361058+murka91@users.noreply.github.com>
2024-05-28 20:14:51 +0600

    Delete aboutus directory

--- aboutus/app.js
+++
@@ -1,8 +0,0 @@
-
-// Бургерное меню
-const burgerMenu = document.querySelector('.burger-menu');
-const navMenu = document.querySelector('.nav-menu');
-
-burgerMenu.addEventListener('click', () => {
-  navMenu.style.display = navMenu.style.display === 'none' ? 'block' : 'none';
-});
